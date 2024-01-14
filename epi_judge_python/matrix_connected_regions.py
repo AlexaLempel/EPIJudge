1ddc5dba8598667346f92acf644681ee579f5a0e
AlexaLempel <Alexa.lempel@gmail.com>
2024-01-14 15:06:26 -0700

    more graph problems

--- epi_judge_python/matrix_connected_regions.py
+++ epi_judge_python/matrix_connected_regions.py
@@ -1,10 +1,33 @@
+from collections import deque
 from typing import List
 
 from test_framework import generic_test
 
 
 def flip_color(x: int, y: int, image: List[List[bool]]) -> None:
-    # TODO - you fill in here.
+    to_visit = deque([(x,y)])
+    color = image[x][y]
+
+    while to_visit:
+        # Get next pixel from queue
+        x,y = to_visit.popleft()
+
+        # Get adjacent pixel coords and add to queue
+        # if in bounds, is the target color, and not yet visited
+        adjacent = [(i,j) for i,j
+                    in map(lambda _x,_y: (_x,_y), 
+                        (x, x, x + 1, x - 1), 
+                        (y + 1, y - 1, y, y))
+                    if 0 <= i < len(image)
+                        and 0 <= j < len(image[i])
+                        and image[i][j] == color]
+        
+        for pixel in adjacent:
+            to_visit.append(pixel)
+        
+        # Flip current pixel
+        image[x][y] = not color
+        
     return
 
 
--- epi_judge_python/matrix_enclosed_regions.py
+++ epi_judge_python/matrix_enclosed_regions.py
@@ -1,10 +1,35 @@
+from collections import deque
 from typing import List
 
 from test_framework import generic_test
 
 
 def fill_surrounded_regions(board: List[List[str]]) -> None:
-    # TODO - you fill in here.
+    m, n = len(board), len(board[0])
+
+    # Add all W squares on the edge to unenclosed set
+    unenclosed = {(i,j) for i,j in [(i,j) for k in range(n) for i,j in ((0, k), (m-1, k))] 
+                  + [(i,j) for k in range(m) for i,j in ((k, 0), (k, n-1))]
+                  if board[i][j] == 'W'}
+
+    to_visit = deque(unenclosed)
+
+    # W squares connected to squares in queue are also unenclosed
+    # Find them with BFS
+    while to_visit:
+        x,y = to_visit.popleft()
+        unenclosed.add((x,y))
+
+        for i,j in ((x, y+1), (x, y-1), (x + 1, y), (x-1, y)):
+            if (0 <= i < m and 0 <= j < n and board[i][j] == "W"
+                and not (i,j) in unenclosed):
+                to_visit.append((i,j))
+
+    for i in range(m): 
+        for j in range(n):
+            if not (i,j) in unenclosed:
+                board[i][j] = 'B'
+        
     return
 
 
